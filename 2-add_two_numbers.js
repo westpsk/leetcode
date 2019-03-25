0aecc6812f61adcb705dea6a3d9809edf7eef2c3
pansongkui <pansongkui@360.cn>
2019-03-26 00:39:31 +0800

    add two numbers

--- 2-add_two_numbers.js
+++ 2-add_two_numbers.js
@@ -11,29 +11,22 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-  let val1 = l1.val
-  let val2 = l2.val
-  let next1 = l1.next
-  let next2 = l2.next
-  let carry = 0
-  const addTwo = (num1, num2) => {
-    let addNum = num1 + num2 + carry
-    carry = Math.floor(addNum/10)
-    return new ListNode(addNum%10)
+  var newList = new ListNode(0)
+  var currList = newList
+  var carry = 0
+  var twoSum = 0
+  newList.next = currList
+  while(l1 || l2 || carry !== 0){
+    var num1 = l1 ? l1.val : 0
+    var num2 = l2 ? l2.val : 0
+    l1 = l1 ? l1.next : null
+    l2 = l2 ? l2.next : null
+    twoSum = carry + num1 + num2
+    carry = twoSum >= 10 ? 1 : 0
+    currList.next = new ListNode(twoSum%10)
+    currList = currList.next
   }
-  
-  let addListNode = addTwo(val1, val2)
-  let node = addListNode
-  while(next1 !== null || next2 !== null || carry !== 0){
-    let num1 = next1 ? next1.val : 0
-    let num2 = next2 ? next2.val : 0
-    let nextListNode = addTwo(num1, num2)
-    node.next = nextListNode
-    node = nextListNode
-    next1 = next1 ? next1.next : null
-    next2 = next2 ? next2.next : null
-  }
-  return addListNode
+  return newList.next
 };
 
 var l1 = [1,2,3]
