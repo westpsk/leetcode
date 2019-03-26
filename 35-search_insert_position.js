2b6669ae36880c5902c14e78e47bfd01d3db6482
pansongkui <pansongkui001@lianjia.com>
2019-03-26 20:23:36 +0800

    add readme

--- 35-search_insert_position.js
+++ 35-search_insert_position.js
@@ -6,7 +6,6 @@
 var find = function(nums, target){
   let len = nums.length
   if(len < 2){
-    console.log('nums=>', nums[0])
     return {
       num: nums[0],
       small: nums[0] > target ? true : false
@@ -15,20 +14,17 @@ var find = function(nums, target){
   if(nums[Math.floor(len/2)-1] > target){
     nums = nums.slice(0, Math.floor(len/2))
   }else{
-    console.log(nums.slice(Math.floor(len/2)))
     nums = nums.slice(Math.floor(len/2))
   }
   return find(nums, target)
 }
 
 var searchInsert = function(nums, target) {
-  console.log(~nums.indexOf(target))
   if(~nums.indexOf(target)){
     return nums.indexOf(target)
   }
   
   const preTarget = find(nums, target)
-  console.log('preTarget=>', preTarget)
   var index = preTarget.small ? nums.indexOf(preTarget.num) : nums.indexOf(preTarget.num) + 1
   return index
 };
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # leetcode
+
+1. [two sum](https://leetcode.com/problems/two-sum/)
+2. [3Sum](https://leetcode.com/problems/3sum/)
+3. [Add Two Numbers](https://leetcode.com/problems/add-two-numbers/)
+4. [Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/)
+5. [Search Insert Position](https://leetcode.com/problems/search-insert-position/)
+6. [Letter Combinations of a Phone Number](https://leetcode.com/problems/letter-combinations-of-a-phone-number/)
