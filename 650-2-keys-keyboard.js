0c890712172f06ee6b8be0f31205529d49d5ee06
ivan <pansongkui@126.com>
2019-04-01 00:43:40 +0800

    优化一下

--- 650-2-keys-keyboard.js
+++ 650-2-keys-keyboard.js
@@ -3,44 +3,18 @@
  * @return {number}
  */
 var minSteps = function(n) {
-  const MAX = 1000
-  if(n < 1 || n > MAX) return false
-  const primeList = findPrime(MAX)
-  let mutiList = []
-  mutiList = findMuti(n, primeList, mutiList)
-  const step = mutiList.reduce((acc, cur) => {
-    return acc + cur
-  }, 0)
-  return step
-};
-
-const findMuti = (n, primeList, mutiList) => {
-  console.log(mutiList)
-  for(let prime of primeList){
-    if(n%prime === 0){
-      mutiList.push(prime)
-      findMuti(n/prime, primeList, mutiList)
-      break
+  if(n === 1) return 0
+  let res = 0
+  for(let i = 2; i <= Math.sqrt(n); i++){
+    console.log('i', i)
+    if(n%i === 0){
+      res += i
+      n /= i
+      i--
     }
   }
-  return mutiList
-}
+  return res + n
+};
 
-const findPrime = (max) => {
-  let primeList = []
-  for(let n = 2; n <= max; n++){
-    isPrime(n) && primeList.push(n)
-  }
-  return primeList
-}
-
-const isPrime = (n) => {
-  let flag = true
-  for(let i = 2; i <= Math.ceil(n/2); i++){
-    if(n%i === 0) flag = false 
-  }
-  return flag
-}
-
-const res = minSteps(10)
+const res = minSteps(12)
 console.log(res)
