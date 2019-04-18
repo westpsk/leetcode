e47278725a3c253c3b5e928ecf382d81d5071f06
ivan <pansongkui@126.com>
2019-04-18 22:37:13 +0800

    翻转二叉树done

---
+++ 226-invert-binary-tree.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if(!root) return root
+  let temp = root.left
+  root.left = root.right
+  root.right = temp
+  if(root.left){
+    invertTree(root.left)
+  }
+  if(root.right){
+    invertTree(root.right)
+  }
+  return root
+};
+
+const res = invertTree([4,2,7,1,3,6,9])
+console.log(res)
