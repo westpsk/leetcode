1b4ce4c38d2bb45053a68739ce140dec4fe3279b
ivan <pansongkui@126.com>
2019-04-25 23:33:56 +0800

    dp 实现构造二叉树

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @param {number[]} inorder
+ * @return {TreeNode}
+ */
+const buildTree = function(preorder, inorder) {
+  return dp(0, preorder.length - 1, preorder, 0, inorder.length - 1, inorder)
+};
+
+const dp = function(s1, e1, preorder, s2, e2, inorder){
+  if(s1 > e1 || s2 > e2) return null
+  let value = preorder[s1]
+  let node = new TreeNode(value)
+  let point = inorder.indexOf(value)
+  let left = point - s2
+  let right = e2 - point
+  node.left = dp(s1+1, s1+left, preorder, s2, s2+left-1, inorder)
+  node.right = dp(e1-right+1, e1, preorder, e2-right+1, e2, inorder)
+  return node
+}
+
