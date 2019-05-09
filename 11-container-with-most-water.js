e24d65beb64c31ca86a200c1f2a5db4c6439d881
pansongkui <pansongkui@126.com>
2019-05-09 20:03:20 +0800

    done

--- 11-container-with-most-water.js
+++ 11-container-with-most-water.js
@@ -6,7 +6,7 @@ var maxArea = function(height) {
   let max = 0
   let len = height.length
   for(let i = 0; i < len - 1; i++){
-    for(let j = len - 1; j > i; j++){
+    for(let j = len - 1; j > i; j--){
       let temp = (j - i) * Math.min(height[i], height[j])
       if(temp > max) {
         max = temp
