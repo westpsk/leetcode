28aea66d97936e30bafb39362116dde2e7f67541
pansongkui <pansongkui@126.com>
2019-05-09 20:25:15 +0800

    优化算法 faster than 100%

--- 11-container-with-most-water.js
+++ 11-container-with-most-water.js
@@ -2,16 +2,33 @@
  * @param {number[]} height
  * @return {number}
  */
+// var maxArea = function(height) {
+//   let max = 0
+//   let len = height.length
+//   for(let i = 0; i < len - 1; i++){
+//     for(let j = len - 1; j > i; j--){
+//       let temp = (j - i) * Math.min(height[i], height[j])
+//       if(temp > max) {
+//         max = temp
+//       }
+//     }
+//   }
+//   return max
+// };
+
 var maxArea = function(height) {
-  let max = 0
+  let res = temp = 0
   let len = height.length
-  for(let i = 0; i < len - 1; i++){
-    for(let j = len - 1; j > i; j--){
-      let temp = (j - i) * Math.min(height[i], height[j])
-      if(temp > max) {
-        max = temp
-      }
+  for(let i = 0, j = len - 1; i < j; ){
+    let temp = (j - i) * Math.min(height[i], height[j])
+    if(temp > res) {
+      res = temp
+    }
+    if(height[i] < height[j]){
+      i++
+    }else{
+      j--
     }
   }
-  return max
+  return res
 };
