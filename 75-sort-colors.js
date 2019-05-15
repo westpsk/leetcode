1ced9825a5ae8537b52a320d98952b859760f317
pansongkui <pansongkui@126.com>
2019-05-15 20:19:46 +0800

    优化

--- 75-sort-colors.js
+++ 75-sort-colors.js
@@ -2,15 +2,35 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function(nums) {
-  for(let i = 0; i < nums.length; i++){
-    for(let j = i + 1; j < nums.length; j++){
-      if(nums[i] > nums[j]){
-        [nums[i], nums[j]] = [nums[j], nums[i]]
+// const sortColors = function(nums) {
+//   for(let i = 0; i < nums.length; i++){
+//     for(let j = i + 1; j < nums.length; j++){
+//       if(nums[i] > nums[j]){
+//         [nums[i], nums[j]] = [nums[j], nums[i]]
+//       }
+//     }
+//   }
+//   return nums
+// };
+
+// tow pointers
+const sortColors = function(nums) {
+  let start = 0
+  let end = nums.length - 1
+  for(let i = 0; i <= end; i++){
+    if(start < end){
+      if(nums[i] === 2) {
+        [nums[i], nums[end]] = [nums[end], nums[i]]
+        end--
+        i--
+      }else if(nums[i] === 0){
+        [nums[i], nums[start]] = [nums[start], nums[i]]
+        start++
       }
     }
   }
   return nums
-};
-let res = sortColors([2,0,2,1,1,0])
+}
+
+let res = sortColors([1, 2, 0])
 console.log(res)
