3548ab3cb429f4ff6b64624ee85f3d31a59e7e40
ivan <pansongkui@126.com>
2019-05-24 00:05:34 +0800

    增减字符串匹配优化

--- 942-di-string-match.js
+++ 942-di-string-match.js
@@ -6,19 +6,14 @@ var diStringMatch = function(S) {
   let res = []
   let min = 0
   let max = S.length
-  for(let i = 0; i < S.length; i++){
-    if(S.charAt(i) === 'I'){
+  for(let i = 0; i <= S.length; i++){
+    if(S[i] === 'I'){
       res[i] = min
       min = min + 1
-    }else if(S.charAt(i) === 'D'){
+    }else{
       res[i] = max
       max = max - 1
     }
-  }
-  if(S.charAt(S.length - 1) === 'I'){
-    res[S.length] = min
-  }else{
-    res[S.length] = max
   }
   return res
 }
