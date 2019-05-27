c8d3cf502794902da053652b28570be84cb982bd
ivan <pansongkui@126.com>
2019-05-28 00:05:33 +0800

    Max(KlogK, N)

--- 1054-distant-barcodes.js
+++ 1054-distant-barcodes.js
@@ -3,15 +3,24 @@
  * @return {number[]}
  */
 var rearrangeBarcodes = function(barcodes) {
-  let list = barcodes.slice().sort((a, b) => a - b)
-  let len = list.length
-  let pointer = 1
-  for(let i = 1; i < list.length/2; ){
-    [list[i], list[len-pointer]] = [list[len-pointer], list[i]]
-    i = i+2
-    pointer++
+  let map = {}
+  let len = barcodes.length
+  for(let i = 0; i < len; i++){
+    if(map[barcodes[i]]){
+      map[barcodes[i]]++
+    }else{
+      map[barcodes[i]] = 1
+    }
   }
-  return list
+  const keys = Object.keys(map).sort((a, b) => map[a] - map[b])
+  let idx = 1
+  for(let k of keys){
+    let t = map[k]
+    for(let i = 0; i < t; i++){
+      if(idx >= barcodes.length) idx = 0
+      barcodes[idx] = k
+      idx += 2
+    }
+  }
+  return barcodes
 }
-const res = rearrangeBarcodes([1,1,1,1,2,2,3,3])
-console.log(res)
