33bb7cc8f5f007ac9f80696874c0994f5dbf2035
ivan <pansongkui@126.com>
2019-06-02 23:29:08 +0800

    reverse link

--- 92-reverse-linked-list-ii.js
+++ 92-reverse-linked-list-ii.js
@@ -12,5 +12,19 @@
  * @return {ListNode}
  */
 var reverseBetween = function(head, m, n) {
-  
-};
+  let dummy = new ListNode(0)
+  dummy.next = head
+  let pre = dummy
+  for(let i = 0; i < m-1; i++){
+    pre = pre.next
+  }
+  let start = pre.next
+  let then = start.next
+  for(let i = 0; i < n - m; i++){
+    start.next = then.next
+    then.next = pre.next
+    pre.next = then
+    then = start.next
+  }
+  return dummy.next
+}
