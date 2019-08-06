6086c36e39043cf54b9698f602db8e393e58913e
ivan <pansongkui@126.com>
2019-08-06 23:19:31 +0800

    math max remove first item

--- 56-merge-intervals.js
+++ 56-merge-intervals.js
@@ -3,13 +3,13 @@
  * @return {number[][]}
  */
 const merge = function (intervals) {
-  if (intervals.length < 2) return intervals
+  if (intervals.length < 1) return intervals
   let sorted = intervals.sort((a, b) => a[0] - b[0])
   let pointer = 0
   sorted.push(sorted[sorted.length - 1])
   while (pointer < sorted.length - 1) {
     if (sorted[pointer][1] >= sorted[pointer + 1][0]) {
-      const max = Math.max(sorted[pointer][1], sorted[pointer + 1][0], sorted[pointer + 1][1])
+      const max = Math.max(sorted[pointer][1], sorted[pointer + 1][1])
       sorted.splice(pointer, 2, [sorted[pointer][0], max])
     } else {
       pointer++
@@ -17,5 +17,5 @@ const merge = function (intervals) {
   }
   return sorted
 }
-const res = merge([[2, 3], [4, 5], [6, 7], [8, 9], [1, 10]])
+const res = merge([[2, 3]])
 console.log(res)
