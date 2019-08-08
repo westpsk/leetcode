b865d81b9f2e91d3a7beee54c0554182e38213b0
ivan <pansongkui@126.com>
2019-08-09 00:41:20 +0800

    解决重复问题

--- 18-4sum.js
+++ 18-4sum.js
@@ -5,25 +5,26 @@
  */
 const fourSum = function (nums, target) {
   let res = []
-  dfs(nums, 0, [], res, 4, target)
+  dfs(nums.sort((a, b) => a - b), 0, [], res, 4, target)
   return res
 }
 
 const dfs = (arr, index, sub, res, num, target) => {
-  if (sub.length === num && sum(sub) === target) {
+  if (sub.length === num && target === 0) {
     return res.push(sub.slice())
   }
   if (sub.length > num) return
   for (let i = index; i < arr.length; i++) {
+    if (i > index && arr[i] === arr[i - 1]) {
+      continue
+    }
     sub.push(arr[i])
-    dfs(arr, i + 1, sub, res, num, target)
+    dfs(arr, i + 1, sub, res, num, target - arr[i])
     sub.pop()
   }
 }
 
-const sum = (arr) => {
-  return arr.reduce((acc, cur) => acc + cur)
-}
-
-const res = fourSum([3, 2, 4, 5, 1], 14)
+const arr = [1, 3, 4, 5, -1]
+const target = 9
+const res = fourSum(arr, target)
 console.log(res)
