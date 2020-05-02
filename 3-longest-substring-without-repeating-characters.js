35ad3c9d0e65a5f5aaabeb504452b71edb65c4d1
ivan <pansongkui@126.com>
2020-05-02 20:29:17 +0800

    update: slide window

--- 3-longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters.js
@@ -2,16 +2,31 @@
  * @param {string} s
  * @return {number}
  */
-const lengthOfLongestSubstring = function(s) {
+const lengthOfLongestSubstring = function (s) {
   let result = 0,
-      i = 0
-  let map = {}
-  for(let j = 0; j < s.length; j++) {
-    if(map[s[j]]) {
-      i = Math.max(map[s[j]], i)
+    i = 0;
+  let map = {};
+  for (let j = 0; j < s.length; j++) {
+    if (map[s[j]]) {
+      i = Math.max(map[s[j]], i);
     }
-    result = Math.max(result, j - i + 1)
-    map[s[j]] = j + 1
+    result = Math.max(result, j - i + 1);
+    map[s[j]] = j + 1;
   }
-  return result
-}
+  return result;
+};
+
+// slide window
+const lengthOfLongestSubstring = function (s) {
+  let slideArr = [];
+  let max = 0;
+  for (let i = 0; i < s.length; i++) {
+    const index = slideArr.indexOf(s[i]);
+    if (~index) {
+      slideArr.splice(0, index + 1);
+    }
+    slideArr.push(s[i]);
+    max = Math.max(max, slideArr.length);
+  }
+  return max;
+};
