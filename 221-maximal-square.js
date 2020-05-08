79149f7789ae63f11595d506e2d03b83226c5a21
ivan <pansongkui@126.com>
2020-05-09 01:14:43 +0800

    feat: dp

--- 221-maximal-square.js
+++ 221-maximal-square.js
@@ -2,21 +2,32 @@
  * @param {character[][]} matrix
  * @return {number}
  */
-var maximalSquare = function (matrix) {
-  if (matrix.length) return 0;
+const maximalSquare = function (matrix) {
   let long = matrix.length;
   let width = matrix[0] && matrix[0].length;
-  let map = Array.from(new Array(long + 1)).fill(new Array(width + 1).fill(0));
+  let map = Array.from(new Array(long)).fill(new Array(width).fill(0));
   let max = 0;
-  for (let i = 1; i < long; i++) {
-    for (let j = 1; j < width; j++) {
-      if (matrix[i][j]) {
-        map[i][j] =
-          Math.min(matrix[i - 1][j], matrix[i - 1][j - 1], matrix[i][j - 1]) +
-          1;
-        max = map[i][j] > max ? max[i][j] : max;
+  for (let i = 0; i < long; i++) {
+    map[i] = new Array(width).fill(0);
+    for (let j = 0; j < width; j++) {
+      let left, top, cross;
+      top = i === 0 ? 0 : map[i - 1][j];
+      left = j === 0 ? 0 : map[i][j - 1];
+      cross = i === 0 || j === 0 ? 0 : map[i - 1][j - 1];
+      if (matrix[i][j] != 0) {
+        map[i][j] = Math.min(left, top, cross) + 1;
       }
+      max = map[i][j] > max ? map[i][j] : max;
     }
   }
-  return max;
+  return max * max;
 };
+
+let input = [
+  ["1", "0", "1", "0", "0"],
+  ["1", "0", "1", "1", "1"],
+  ["1", "1", "1", "1", "1"],
+  ["1", "0", "0", "1", "0"],
+];
+const res = maximalSquare(input);
+console.log(res);
