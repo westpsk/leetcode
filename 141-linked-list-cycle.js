7dc250b4e221c92957544b5166474ec52bf4e9ec
ivan <pansongkui@126.com>
2020-05-10 01:43:03 +0800

    feat: check link cycle

---
+++ 141-linked-list-cycle.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const hasCycle = function (head) {
+  while (head) {
+    if (head.tag) {
+      return true;
+    }
+    head.tag = true;
+    head = head.next;
+  }
+  return false;
+};
+
+const hasCycle = function (head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+};
