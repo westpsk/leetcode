73ab6c30a4fc76eef0f4e0d27e4f8ff449d48cf2
ivan <pansongkui@126.com>
2020-05-13 00:55:11 +0800

    feat: add new version

--- 50-powx-n.js
+++ 50-powx-n.js
@@ -3,8 +3,25 @@
  * @param {number} n
  * @return {number}
  */
-var myPow = function (x, n) {
+
+// 递归
+const myPow = function (x, n) {
   if (n === 0) return 1;
   if (n < 0) return 1 / myPow(x, -n);
   return n % 2 == 0 ? myPow(x * x, n / 2) : x * myPow(x, n - 1);
+};
+
+// 二分
+const myPow = function (x, n) {
+  if (n == 0) return 1;
+  let pow = Math.abs(n);
+  let res = 1;
+  while (pow) {
+    if (pow % 2 === 1) {
+      res *= x;
+    }
+    pow = Math.floor(pow / 2);
+    x *= x;
+  }
+  return n > 0 ? res : 1 / res;
 };
