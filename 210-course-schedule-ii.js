c1003737721838e66417ac8399465f3152b2d9b1
ivan <pansongkui@126.com>
2020-05-19 23:53:45 +0800

    feat: 拓扑排序 dfs

--- 210-course-schedule-ii.js
+++ 210-course-schedule-ii.js
@@ -4,4 +4,36 @@
  * @return {number[]}
  */
 // https://www.youtube.com/watch?v=x3mm5a_CwRM
-var findOrder = function (numCourses, prerequisites) {};
+const findOrder = (numCourses, prerequisites) => {
+  // 拓扑排序，顶点 map
+  const vertex = [...Array(numCourses)].map((r) => []);
+  const res = [];
+  for (let [u, v] of prerequisites) {
+    vertex[v].push(u);
+  }
+  for (let i = 0; i < numCourses; i++) {
+    if (!dfs(i, vertex, new Set(), new Set(), res)) {
+      return [];
+    }
+  }
+  return res.reverse();
+};
+
+const dfs = (v, vertex, seen, seeing, res) => {
+  if (seen.has(v)) {
+    return true;
+  }
+  if (seeing.has(v)) {
+    return false;
+  }
+  seeing.add(v);
+  for (let nv of vertex[v]) {
+    if (!dfs(nv)) {
+      return false;
+    }
+  }
+  seeing.delete(v);
+  seen.add(v);
+  res.push(v);
+  return true;
+};
