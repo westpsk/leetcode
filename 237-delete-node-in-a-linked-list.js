e84b4b924d6cd3a2872cf6dc236c1af4895de299
ivan <pansongkui@126.com>
2020-06-10 23:51:54 +0800

    feat: delete link node

---
+++ 237-delete-node-in-a-linked-list.js
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+const deleteNode = function (head) {
+  head.val = head.next.val;
+  head.next = head.next.next;
+};
